892d6b96b204d0348552ced88c5a43f810a50e42
Jie Hou <namehb@gmail.com>
2024-11-24 14:27:30 +0100

    exercise0 update

--- exercises/exercise0/1_hemnet/sql/query_hemnet.sql
+++ exercises/exercise0/1_hemnet/sql/query_hemnet.sql
@@ -3,20 +3,67 @@
 -- b) Make a wildcard selection to checkout the data,  * 通配符选择 wildcard selection
 SELECT * from main.hemnet_data limit 5;
 
+-- testing percentage_difference
+SELECT 6950000*0.07;
+-- 486500.00
+SELECT 6590000 - 6950000*0.07;
+-- Percentage Difference= (Final Price−Asked Price) /Asked Price ×100
+
 
 -- c) Find out how many rows there are in the table
-select COUNT(*) from main.hemnet_data; 
+select COUNT(*) as total_rows from main.hemnet_data; 
 
 -- d) Describe the table that you have ingested to see the columns and data types.
 DESCRIBE main.hemnet_data;
 
+desc;
+desc table main.hemnet_data ;
+
 -- e) Find out the 5 most expensive homes sold.
+
 SELECT * from main.hemnet_data order by final_price desc limit 5;
 
+SELECT
+	address,
+	commune,
+	final_price
+from
+	main.hemnet_data
+order by
+	final_price desc
+limit 5;
+
+
 -- f) Find out the 5 cheapest homes sold.
+
 SELECT * from main.hemnet_data order by final_price asc limit 5;
 
+SELECT
+	address,
+	commune,
+	area,
+	rooms,
+	final_price
+from
+	main.hemnet_data
+order by
+	final_price
+limit 5;
+
+-- explore rooms
+
+SELECT
+	Rooms,
+	area,
+	final_price
+from
+	main.hemnet_data hd
+WHERE
+	rooms <= 3;
+
+
 -- g) Find out statistics on minimum, mean, median and maximum prices of homes sold.
+
 SELECT 
 	MIN(final_price) AS min_price,
 	round(AVG(final_price)) AS mean_price,
@@ -25,10 +72,17 @@ SELECT
 FROM 
 	main.hemnet_data;
 
--- median(final_price) AS median_price
+SELECT 
+	MIN(final_price) AS min_price,
+	round(AVG(final_price)) AS mean_price,
+	median(final_price) AS median_price,
+	MAX(final_price) AS max_price
+FROM 
+	main.hemnet_data;
 
 
 -- h) Find out statistics on minimum, mean, median and maximum prices of price per area.
+
 SELECT 
 	MIN(price_per_area) AS min_price_area,
 	round(AVG(price_per_area)) AS mean_price_area,
@@ -43,11 +97,26 @@ SELECT COUNT(DISTINCT commune) from main.hemnet_data;
 
 
 -- j) How many percentage of homes cost more than 10 million?
+-- (delen/hela)*100
+-- antagande 1 rad  -> 1 hem
 
 SELECT
     (SELECT COUNT(*) FROM main.hemnet_data WHERE final_price > 10000000) * 100.0 / COUNT(*) AS percentage
 FROM
     main.hemnet_data;
+   
+
+  
+ -- percent_over_10000000 
+   
+SELECT COUNT(*) FROM main.hemnet_data;   -- 500
+   
+ SELECT
+	COUNT(*)/500*100 as percent_over_10m 
+FROM
+	main.hemnet_data
+WHERE
+	final_price > 10000000;  
    
 
 -- k) Feel free to explore anything else you find interesting in this dataset.
